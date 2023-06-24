2e671095ec1a24a7b3fd9f3a75bca7c800c9685b
DimanaVyatrova <dimana.vqtrova@gmail.com>
2023-06-24 20:51:18 +0300

    ms

--- backend/api/date_signup.php
+++ backend/api/date_signup.php
@@ -2,19 +2,11 @@
     require_once("../db/db.php");
 
     $userData = json_decode(file_get_contents("php://input"), true);
-    //var_dump($userData);
-
-    //echo json_encode(array("name" => "didi", "age" => "midi"));
 
     try {
 
         $db = new DB();
         $connection = $db->getConnection();
-
-        //$roles_id = getUsersRoleId($connection);
-        //$userData += ["roles_id" => $roles_id];
-        // $userDataSQL = ["name" => $userData["name"], "faculty-number" => $userData["faculty-number"], "topic" => $userData["topic"],
-        // "date" => $userData["date"], "hour" => $userData["hour"]];
 
          $sql = "INSERT INTO presentations (name, faculty_number, topic, date, hour, field, technology) 
                  VALUES (:name, :faculty_number, :topic, :date, :hour, :field, :technology)";
--- frontend/search_page/search_page.js
+++ frontend/search_page/search_page.js
@@ -1,6 +1,10 @@
 const searchForm = document.getElementById('search-form');
 
+console.log("asdsdf");
+
 searchForm.addEventListener('submit', (event) => {
+    console.log("asdsdf");
+    
     const userData = {};
     const selectors = signupForm.querySelectorAll('select');
     selectors.forEach(selector => {
